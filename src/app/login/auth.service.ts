b3a59b68830ce74b0b2abbc3b2acb8c6ba0ae0a9
Mohtadi Bakali Tahiri <teddybak@hotail.com>
2020-08-01 22:34:28 +0100

    adding response interface

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -2,23 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, EventEmitter } from '@angular/core';
 import { Usuario } from './usuario';
 import { Router } from '@angular/router';
- 
+
+import { ApiResponse } from '../interfaces/apiResponse.interface';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private usuarioAutenticado = false;
   public emisor = new EventEmitter<boolean>();
-  constructor(private router: Router, private http:HttpClient) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
-  usuarioRegistrado:any;
+  usuarioRegistrado: any;
   hacerLogin(usuario: Usuario) {
-
-      this.http.post(`http://localhost:3000/login`,usuario).subscribe(data =>{
-          this.usuarioRegistrado = data;
-      })
-      if(this.usuarioRegistrado == true)
-   {
+    this.http
+      .post<ApiResponse>(`http://localhost:3000/login`, usuario)
+      .subscribe((data) => {
+        this.usuarioRegistrado = data;
+      });
+    if (this.usuarioRegistrado.status == true) {
       this.usuarioAutenticado = true;
       this.emisor.emit(true);
       this.router.navigate(['/']);
